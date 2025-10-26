cbdf93dc271d22b158ce65d413265f93ee66f845
Tomas Lew <139467803+TomasLew@users.noreply.github.com>
2025-10-26 23:39:33 +0000

    Query 12

---
+++ 12_InfoMed_TP4_Lew_Bereterbide.sql
@@ -0,0 +1,8 @@
+SELECT 
+    m.nombre AS medicamento,
+    COUNT(*) AS veces_recetado
+FROM Recetas r
+JOIN Medicamentos m ON r.id_medicamento = m.id_medicamento
+WHERE r.id_medico = 2
+GROUP BY m.nombre
+HAVING COUNT(*) > 1;

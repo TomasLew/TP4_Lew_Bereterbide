733173e7c935fc629242f98cff09285f4ae150fd
Tomas Lew <139467803+TomasLew@users.noreply.github.com>
2025-10-26 23:19:00 +0000

    Query 11

---
+++ 11_InfoMed_TP4_Lew_Bereterbide.sql
@@ -0,0 +1,7 @@
+SELECT 
+    p.nombre AS paciente,
+    c.fecha,
+    c.diagnostico
+FROM Consultas c
+JOIN Pacientes p ON c.id_paciente = p.id_paciente
+WHERE c.fecha BETWEEN '2024-08-01' AND '2024-08-31'
